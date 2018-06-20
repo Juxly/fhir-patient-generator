88992603856b9cd420a92abae793b4618bc121dd
Zach <Zach@juxly.com>
2018-06-20 16:02:14 -0500

    moved cli interface from test to standard and updated readme

--- README.md
+++ README.md
@@ -14,4 +14,27 @@ This is where the instructions should go for how to install this
 
 ## Usage
 
-This is where the instructions should go for how to use this
+This utility is used by navigating to its directory and running the following command
+
+`npm run generate`
+
+Its behavior can be modidfied by adding flags
+
+`npm run generate --  [flags]`
+
+The --verbose, --v flag makes the generater print out the entire patient and condition jsons as they are generated
+
+`npm run generate -- --verbose`
+
+The --quantity, --q flag sets how many patients to generate. It's dafault is 1
+
+`npm run generate -- --quantity=5` generates 5 patients
+
+`npm run generate -- --q=15` generates 5 patients
+
+The --specialty, --s flag sets what specialty to generate patients with. The currently supported specialties are:
+* nuerology
+* hcc
+
+`npm run generate -- --specialty=nuerology` generates 1 nuerology patient
+`npm run generate -- --s=hcc` generates 1 hcc patient
--- generate-patient.js
+++ generate-patient.js
@@ -1,27 +1,72 @@
 #!/usr/bin/env babel-node
-import args from 'args'
+import yargs from 'yargs'
+import lookupConditionSet from './helpers/lookupConditionSet'
 import generatePatient from './generators/patient/patient.generator'
-import savePatient from './helpers/savePatient'
+import generateConditionBundle from './generators/condition/conditionBundle.generator'
+import PatientSaver from './helpers/patientSaver'
 
-args
-  .option('patients', 'How many patients to generate', 1)
-  .option('specialty', 'Which specialty the patient should be generated with', '')
+const argv = yargs
+  .option('specialty', {
+    alias: 's',
+    default: 'None',
+    describe: 'Specifies the Specialty of the patients generated',
+    type: 'string'
+  })
+  .option('quantity', {
+    alias: 'q',
+    default: 1,
+    describe: 'Defines how many patients will be generated',
+    type: 'number'
+  })
+  .option('verbose', {
+    alias: 'v',
+    default: false,
+    describe: 'Program prints patients to console as they are generated',
+    type: 'boolean'
+  })
+  .help().alias('help', 'h')
+  .version('v1.0.0').alias('version', 'V')
+  .argv
 
-const flags = args.parse(process.argv)
-console.log(flags)
-
-for (var i = 0; i < flags.patients; i++) {
-  var patient = generatePatient()
-  console.log(JSON.stringify(patient))
-  savePatient(patient)
-  switch (flags.specialty) {
-    case 'nuerology':
-      console.log('nuerology')
-      break
-    case 'hcc':
-      console.log('hcc')
-      break
-    default:
-      console.log('No known specialty selected')
+function main () {
+  // test flags
+  if (argv.quantity <= 0) {
+    console.log('ERROR: Patient quantity must be greater than 0')
+    return false
   }
+  if (!Number.isSafeInteger(argv.quantity)) {
+    console.log('ERROR: Patient quantity must be an integer')
+    return false
+  }
+  if (typeof lookupConditionSet(argv.specialty) === 'undefined' && argv.specialty !== 'None') {
+    console.log(`ERROR: "${argv.specialty}" is not a known specialty`)
+    return false
+  }
+
+  // main generation loop
+  for (var i = 1; i <= argv.quantity; i++) {
+    console.log('\nGenerating patient ' + i)
+
+    // generate patient, print if in verbose mode
+    var patient = generatePatient()
+    if (argv.verbose) console.log('\n\tpatient.json\n' + JSON.stringify(patient))
+
+    // generate conditions, print if in verbose mode
+    var conditionSet = lookupConditionSet(argv.specialty)
+    var conditions = generateConditionBundle(patient, conditionSet)
+    if (argv.verbose) console.log('\n\tcondition.json\n' + JSON.stringify(patient))
+
+    // create PatientSaver object and save patient to file
+    var patientSaver = new PatientSaver(patient)
+    if (typeof conditions !== 'undefined') {
+      patientSaver.addConditions(conditions)
+      patientSaver.saveAll()
+    } else {
+      patientSaver.savePatient()
+    }
+    console.log(`\tPatient ${i} complete`)
+  }
+  console.log('Complete. All patients generated')
 }
+
+main()
